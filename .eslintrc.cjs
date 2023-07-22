6a79da5015857e78ee33db221a542041812c8c9c
Scott A. Matthews <scott@designsbysm.com>
2023-07-21 19:21:42 -0700

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react/jsx-runtime",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.app.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["react-refresh"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+  },
+  overrides: [
+    {
+      files: ["*.jsx", "*.tsx"],
+      rules: {
+        "react/react-in-jsx-scope": "off",
+      },
+    },
+  ],
+};
